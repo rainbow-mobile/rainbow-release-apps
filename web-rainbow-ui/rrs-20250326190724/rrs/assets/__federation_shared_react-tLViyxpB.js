31e535fc243a11ff7c47d761f83e630b80092b14
dfd1123 <dfd1123@naver.com>
2025-03-26 19:42:22 +0900

    remove: 불필요한 파일들 정리

--- web-rainbow-ui/rrs-20250326190724/rrs/assets/__federation_shared_react-tLViyxpB.js
+++
@@ -1,1 +0,0 @@
-import{r,g as t}from"./index-zCPSE6if.js";var e=r();const o=t(e);export{o as default};
--- web-rainbow-ui/rrs/RBQ-dev/rrs-RBQ-dev-20250326180411/assets/__federation_shared_react-dom-CN1e5ESY.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./index-zCPSE6if.js";import{r as o}from"./index-CjeWTpq4.js";var t=o();const m=r(t);export{m as default};
